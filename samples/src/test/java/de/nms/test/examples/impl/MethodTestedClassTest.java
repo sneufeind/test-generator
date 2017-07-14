abb4bc349cfe7e0920b1c04187297bdcf6b4e734
Sven Neufeind <ps.neufeind@gmail.com>
2017-07-14 17:51:24 +0200

    Add SCM for relaese

--- samples/src/test/java/de/nms/test/examples/impl/MethodTestedClassTest.java
+++ samples/src/test/java/de/nms/test/examples/impl/MethodTestedClassTest.java
@@ -1,21 +1,23 @@
 package de.nms.test.examples.impl;
 
-import java.lang.Exception;
-import javax.annotation.Generated;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import javax.annotation.Generated;
+
+@Ignore("examples for unit test generating")
 @Generated(
-    value = "de.nms.test.apt.generator.TestCaseGenerator",
-    date = "06.07.17 07:13"
+        value = "de.nms.test.apt.generator.TestCaseGenerator",
+        date = "06.07.17 07:13"
 )
 public class MethodTestedClassTest {
-  @Test
-  public void testDoSomething() throws Exception {
-    // Arrange
+    @Test
+    public void testDoSomething() throws Exception {
+        // Arrange
 
-    // Act
+        // Act
 
-    // Assert
-    org.junit.Assert.fail("'testDoSomething' not implemented yet!");
-  }
+        // Assert
+        org.junit.Assert.fail("'testDoSomething' not implemented yet!");
+    }
 }
